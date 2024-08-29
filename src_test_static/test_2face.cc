e1b436a424e272508172cbcce00007d035e9b53b
Martin, Oberzalek <martin.oberzalek@anton-paar.com>
2024-08-29 10:34:55 +0200

    enabled all test cases

--- src_test_static/test_2face.cc
+++ src_test_static/test_2face.cc
@@ -58,14 +58,14 @@ int main( int argc, char **argv )
 #endif
 
 		std::vector<std::shared_ptr<TestCaseBase<bool>>> test_cases;
-/*
+
 		test_cases.push_back( test_case_static_TwoFace_write1() );
 		test_cases.push_back( test_case_static_TwoFace_write2() );
 		test_cases.push_back( test_case_static_TwoFace_write3() );
 
 		test_cases.push_back( test_case_static_TwoFace_delete1() );
 		test_cases.push_back( test_case_static_TwoFace_rename1() );
-		*/
+
 		test_cases.push_back( test_case_static_TwoFace_list_files1() );
 
 		ColBuilder col;
